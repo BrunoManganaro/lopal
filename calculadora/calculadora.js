64f24d6ac4e785aa976699ba3c47f8dd8b54544d
BrunoManganaro <brunomanganaro0707@gmail.com>
2026-03-04 16:50:41 -0300

    feat: Opções de encerramento da calculadora

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,9 +1,12 @@
 function calculadora() {
+    
     while(true) {
         let resultado = 0.0;
         let operando1;
         let operando2;
         let operador;
+        let continua;
+        let operador_valido = true;
 
         operando1 = parseFloat(prompt("Digite o primeiro número: "));
         operador = prompt("Digite uma das operações ( + - * / ): ");
@@ -19,15 +22,43 @@ function calculadora() {
         } else if (operador === "/") {
             if (operando2 == 0) {
                 alert("Não é possível dividir por zero")
-                return
+                continua = prompt(
+                    "Digite sim para continuar e não para encerrar")
+
+                if (continua === "não") {
+                return //return força a parada de uma função
+                } 
+            
+                
+
+            
+    
             }
-            resultado = operando1 / operando2;
+        resultado = operando1 / operando2;
         } else {
             alert("Operador inválido!");
-            return //return força a parada de uma função
+            operador_valido = false; 
+            continua = prompt ("Digite sim para continuar e não para encerrar");
+            if (continua === "não") {
+
+                return //return força a parada de uma função
+
+            } 
+            
+    
         }
 
-        alert("Resultado: " +
-            operando1 + " " + operador + " " + operando2 + " = " + resultado);
+        if (operador != "/" || operando2 != 0) {
+            alert("Resultado: " +
+                operando1 + 
+                " " + 
+                operador + 
+                " " + 
+                operando2 + 
+                " = " + 
+                resultado);
+
+        }
+        // o símbolo || significa "ou" 
     }
-}
+} 
